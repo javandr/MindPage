bffaead496954996bb90fc4983e2c1ba7af107e2
javandr <javandr.p@gmail.com>
2023-03-29 18:26:23 +0700

    nath

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// main.js
+
+// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
@@ -12,41 +15,11 @@ const createWindow = () => {
     }
   })
 
-  // Add buttons for closing, minimizing, and maximizing the window
-  mainWindow.setMenu(null)
-  mainWindow.maximize()
-  mainWindow.setFullScreenable(false)
-  mainWindow.setResizable(false)
-  mainWindow.setClosable(true)
-
-  const closeBtn = document.createElement('button')
-  closeBtn.textContent = '×'
-  closeBtn.classList.add('window-btn')
-  closeBtn.addEventListener('click', () => mainWindow.close())
-  mainWindow.document.body.appendChild(closeBtn)
-
-  const minimizeBtn = document.createElement('button')
-  minimizeBtn.textContent = '_'
-  minimizeBtn.classList.add('window-btn')
-  minimizeBtn.addEventListener('click', () => mainWindow.minimize())
-  mainWindow.document.body.appendChild(minimizeBtn)
-
-  const maximizeBtn = document.createElement('button')
-  maximizeBtn.textContent = '□'
-  maximizeBtn.classList.add('window-btn')
-  maximizeBtn.addEventListener('click', () => {
-    if (mainWindow.isMaximized()) {
-      mainWindow.unmaximize()
-      maximizeBtn.textContent = '□'
-    } else {
-      mainWindow.maximize()
-      maximizeBtn.textContent = '❐'
-    }
-  })
-  mainWindow.document.body.appendChild(maximizeBtn)
-
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -69,3 +42,5 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
